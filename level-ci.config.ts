6abacd5a9c89bfacdbe2922366658f9ef26eeb65
Daria <daria.osokina@levelaccess.com>
2025-12-12 11:19:55 +0200

    adding ngrok in level-ci.config.ts file

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,9 +1,18 @@
 import type { Config } from "@level-ci/cli";
+import { ngrokConnection } from '@level-ci/ngrok';
 export default {
  organization: "daria-osokina-1-levelaccess-com-kofcj",
- project: "level-ci-playwright-sample-github",
+ project: "daria-manual-website-1-mirko-dev",
  token: process.env.LEVEL_CI_TOKEN,
  server: "https://api.dev.userway.dev",
+ connection: ngrokConnection({
+    authtoken: "35mmkGNEJ9rWvXfX3LYE1N67BUG_34NhuwfrhPFsnF88SEYST",
+    addr: 'http://localhost:1342',
+
+    }),
+  pages: [
+      {url: "/index.html", devices: ["desktop"]}
+    ],
  reportPaths: ['./level-ci-reports']
 } satisfies Config;
 
