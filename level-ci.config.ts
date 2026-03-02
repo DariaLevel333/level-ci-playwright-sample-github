ee3a480e421ecfd0b6aab920cb895df84a2edbf5
Daria <daria.osokina@levelaccess.com>
2026-03-02 16:38:55 +0200

    two

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "@level-ci/cli";
+/* import type { Config } from "@level-ci/cli";
 import { ngrokConnection } from '@level-ci/ngrok';
 export default {
  organization: "daria-osokina-1-levelaccess-com-kofcj",
@@ -7,21 +7,21 @@ export default {
  server: "https://api.dev.userway.dev",
  reportPaths: ['./level-ci-reports']
 } satisfies Config;
+ */
 
-
-// import type { Config } from "@level-ci/cli";
-// import { ngrokConnection } from '@level-ci/ngrok';
-// export default {
-//  organization: "daria-osokina-1-levelaccess-com-kofcj",
-//  project: "level-ci-playwright-sample-github",
-//  token: process.env.LEVEL_CI_TOKEN,
-//  server: "https://api.dev.userway.dev",
-//  connection: ngrokConnection({
-//     authtoken: process.env.NGROK_AUTHTOKEN,
-//     addr: 'http://localhost:1342',
-//     }),
-//   pages: [
-//       {url: "/index.html", devices: ["desktop"]}
-//     ],
-//  reportPaths: ['./level-ci-reports']
-// } satisfies Config;
+import type { Config } from "@level-ci/cli";
+import { ngrokConnection } from '@level-ci/ngrok';
+export default {
+ organization: "daria-osokina-1-levelaccess-com-kofcj",
+ project: "level-ci-playwright-sample-github",
+ token: process.env.LEVEL_CI_TOKEN,
+ server: "https://api.dev.userway.dev",
+ connection: ngrokConnection({
+    authtoken: process.env.NGROK_AUTHTOKEN,
+    addr: 'http://localhost:1342',
+    }),
+  pages: [
+      {url: "/index.html", devices: ["desktop"]}
+    ],
+ reportPaths: ['./level-ci-reports']
+} satisfies Config;
